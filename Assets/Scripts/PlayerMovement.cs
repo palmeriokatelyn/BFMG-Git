8df1b2cecf169836e3d52f585dce17ca10ae9b1e
palmeriokatelyn <palmeriokatelyn@gmail.com>
2025-10-15 20:26:33 -0400

    test

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,17 +11,48 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody rb;
     private bool isGrounded = true;
 
+    public float maxPower = 1f;
+    private float currentPower;
+    public float powerForce = 0.5f;
+    private int bubbleCount = 0;
+
+    public GameObject spawnPoint;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb.GetComponent<Rigidbody>();
+        currentPower = maxPower;
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
+
+        if (isGrounded)
+        {
+            if (currentPower < maxPower)
+            {
+                currentPower += Time.deltaTime;
+            }
+        }
         
+
+        Debug.Log("Current power is: " + currentPower);
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetAxisRaw("Jump") > 0f && currentPower > 0f)
+        {
+            Debug.Log("She bubble on my floatie till i Mcgee");
+            currentPower -= Time.deltaTime;
+            rb.AddForce(rb.transform.up * powerForce, ForceMode.Impulse);
+        }
     }
 
     private void Movement()
@@ -43,6 +74,17 @@ public class PlayerMovement : MonoBehaviour
         if (collision.gameObject.tag == ("Ground"))
         {
             isGrounded = true;
+        }
+
+        if (collision.gameObject.tag == ("Bubble"))
+        {
+            bubbleCount++;
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.tag == ("GameOver"))
+        {
+            rb.transform.position = spawnPoint.transform.position;
         }
     }
 }
